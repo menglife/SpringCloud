d26c475da0c8eb8ac672f537c7dfc536d72804ce
zhangmengnan <m18811933011@163.com>
2021-07-19 15:11:36 +0800

    2

---
+++ cloud-config-client-3355/src/main/java/com/zmn/springcloud/MainClientApplication3355.java
@@ -0,0 +1,13 @@
+package com.zmn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MainClientApplication3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(MainClientApplication3355.class,args);
+    }
+}
